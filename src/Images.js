6352d76e443273bdd204b5aaa8e0ada70cdaa080
Luisa Molenda <77063747+molendaluisa@users.noreply.github.com>
2021-04-17 11:26:11 +0200

    mobile version/responsiveness adjusted

--- src/Images.js
+++ src/Images.js
@@ -8,7 +8,7 @@ export default function Images(props) {
       <div className="row">
         {props.img.map(function (img, index) {
           return (
-            <div className="col-4" key={index}>
+            <div className="col-12 col-md-4" key={index}>
               <a href={img.links.html} target="_blank" rel="noreferrer">
                 <img src={img.urls.small} className="rounded" alt=" search results"></img>
               </a>
